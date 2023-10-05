e9f120c0b327b7572cb25f4ddf8dad3929e6f976
Muhammad Khurram <iamkhurramcs@gmail.com>
2023-10-05 13:02:00 +0500

    second commit

--- src/List.js
+++ src/List.js
@@ -4,30 +4,38 @@ import { Link } from 'react-router-dom';
 function List() {
   return (
     <div>
-      <h1>Routes</h1>
-      <ul className='space-y-4'>
-        <li>
-          <Link to="/app" className="text-blue-500 hover:text-red-700">App</Link>
-        </li>
-        <li>
-          <Link to="/qa" className="text-blue-500 hover:text-red-700">Q.A</Link>
-        </li>
-        <li>
-          <Link to="/quotes" className="text-blue-500 hover:text-red-700">Quotes</Link>
-        </li>
-        <li>
-          <Link to="/table" className="text-blue-500 hover:text-red-700">Table</Link>
-        </li>
-        <li>
-          <Link to="/users" className="text-blue-500 hover:text-red-700">Users</Link>
-        </li>
-        <li>
-          <Link to="/video" className="text-blue-500 hover:text-red-700">Video</Link>
-        </li>
-        <li>
-          <Link to="/github" className="text-blue-500 hover:text-red-700">Github</Link>
-        </li>
-      </ul>
+      <div className="flex items-center justify-between">
+        <div className="flex items-center">
+          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTiQI3KZbaArQIp8jmAcHd0kzVPB_dTcGx5jhuHJU8GCN5zYBUYp8m4vA5SxigAsArVnW8&usqp=CAU" alt="Logo" className="h-8 w-8 mr-2" />
+          <h1 className="text-2xl font-bold mb-1">Routes</h1>
+        </div>
+        &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+        <nav style={{ background: 'linear-gradient(to bottom, #000 20%, transparent 20%)', border: '0.5px solid #100', width: '1800px' }}>
+          <ul className="flex list-none">
+            <li className="mr-6">
+              <Link to="/app" className="text-black-500 hover:text-red-900">App</Link>
+            </li>
+            <li className="mr-6">
+              <Link to="/qa" className="text-black-500 hover:text-red-900">Q.A</Link>
+            </li>
+            <li className="mr-6">
+              <Link to="/quotes" className="text-black-500 hover:text-red-900">Quotes</Link>
+            </li>
+            <li className="mr-6">
+              <Link to="/table" className="text-black-500 hover:text-red-900">Table</Link>
+            </li>
+            <li className="mr-6">
+              <Link to="/users" className="text-black-500 hover:text-yellow-900">Users</Link>
+            </li>
+            <li className="mr-6">
+              <Link to="/video" className="text-black-500 hover:text-yellow-900">Video</Link>
+            </li>
+            <li>
+              <Link to="/github" className="text-black-500 hover:text-yellow-900">Github</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
     </div>
   );
 }
